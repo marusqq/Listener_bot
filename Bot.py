846d203c29d188f6413012165c7a51ee143d5da7
Marius Pozniakovas <pozniak.marius@gmail.com>
2020-01-24 02:41:55 +0200

    Adding logic to download pics with topreddit

--- Bot.py
+++ Bot.py
@@ -21,6 +21,21 @@ import smtplib, ssl
 import sys
 
 
+#photos
+from fake_useragent import UserAgent
+import argparse
+import colorama
+import json
+import os
+import re
+import requests
+import sys
+#photos end
+
+#deleting files
+import shutil
+
+
 #-------------------------------------------------------
 #0. start the script
 print ('Listener Started!!!!')
@@ -83,10 +98,108 @@ Subject: """ + subject + """
 
 
 
+def delete_dir(subreddit):
+    if os.path.isdir( 'C:/Users/marius.pozniakovas/Desktop/randomPyScripts/Listener_bot/' + subreddit):
+        shared_files = os.listdir('C:/Users/marius.pozniakovas/Desktop/randomPyScripts/Listener_bot/' + subreddit)
+        if shared_files is not None:
+            shutil.rmtree('C:/Users/marius.pozniakovas/Desktop/randomPyScripts/Listener_bot/' + subreddit)
+
 
 #log example                 
 #log.info("{} will be removed from {}".format(author_id, thread_id))S
 logfile = open('history.log', 'w+')
+
+
+def send_reddit_photos(subreddit, author_id, threadid, threadtype):
+    
+    code_dir = 'C:/Users/marius.pozniakovas/Desktop/randomPyScripts/Listener_bot/' + 'reddit/' 
+    reddit_photos_dir = code_dir + subreddit
+    reddit_photos = os.listdir(reddit_photos_dir)
+    
+    for photo in reddit_photos:
+        
+        client.sendLocalImage(
+                    reddit_photos_dir + '/' + photo,
+                    Message(text='Author: this_guy, photo from: ' + subreddit, mentions=[Mention(author_id, offset=8, length=8)]),
+                    thread_id=threadid,
+                    thread_type=threadtype,
+                )
+        
+#photos
+def get_valid_filename(s):
+    ''' strips out special characters and replaces spaces with underscores, len 200 to avoid file_name_too_long error '''
+    s = str(s).strip().replace(' ', '_')
+    return re.sub(r'[^\w.]', '', s)[:200]
+
+
+def erase_previous_line():
+    # cursor up one line
+    sys.stdout.write("\033[F")
+    # clear to the end of the line
+    sys.stdout.write("\033[K")
+
+
+def get_pictures_from_subreddit(data, subreddit, location):
+    for i in range(len(data)):
+        current_post = data[i]['data']
+        image_url = current_post['url']
+        if '.png' in image_url:
+            extension = '.png'
+        elif '.jpg' in image_url or '.jpeg' in image_url:
+            extension = '.jpeg'
+        elif 'imgur' in image_url:
+            image_url += '.jpeg'
+            extension = '.jpeg'
+        else:
+            continue
+
+        erase_previous_line()
+        print('downloading pictures from r/' + subreddit +
+              '.. ' + str((i*100)//len(data)) + '%')
+
+        # redirects = False prevents thumbnails denoting removed images from getting in
+        image = requests.get(image_url, allow_redirects=False)
+        if(image.status_code == 200):
+            try:
+                output_filehandle = open(
+                    location + '/' + get_valid_filename(current_post['title']) + extension, mode='bx')
+                output_filehandle.write(image.content)
+            except:
+                pass
+
+
+def grab_pic(subreddit, top, image_count):
+    colorama.init()
+    ua = UserAgent(fallback='Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11')
+
+    #for j in range(3):
+    print('Connecting to r/' + subreddit)
+    url = 'https://www.reddit.com/r/' + subreddit + '/top/.json?sort=top&t=' + \
+        top + '&limit=' + image_count
+    response = requests.get(url, headers={'User-agent': ua.random})
+    
+    if os.path.exists('C:/Users/marius.pozniakovas/Desktop/randomPyScripts/Listener_bot/reddit/'):
+        location = os.path.join('C:/Users/marius.pozniakovas/Desktop/randomPyScripts/Listener_bot/reddit/', subreddit)
+
+    else:
+        print('Given path does not exist, try without the location parameter to default to the current directory')
+        exit()
+
+    if not response.ok:
+        print("Error check the name of the subreddit", response.status_code)
+        exit()
+
+    if not os.path.exists(location):
+        os.mkdir(location)
+    # notify connected and downloading pictures from subreddit
+    erase_previous_line()
+    print('downloading pictures from r/' + subreddit + '..')
+
+    data = response.json()['data']['children']
+    get_pictures_from_subreddit(data, subreddit, location)
+    erase_previous_line()
+    print('Downloaded pictures from r/' + subreddit)
+#photos
 
 class ListenerBot(Client):
     def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
@@ -103,6 +216,19 @@ class ListenerBot(Client):
                 self.send(Message(text = 'Hi, I am ListenBot.\nMy God and Creator is Marius Pozniakovas. Check !info for documentation (ᵔᴥᵔ)'), thread_id = thread_id, thread_type = thread_type)
                 log.info('!help used by: {}'.format(author_id))
                 logfile.write('!help used by: {}'.format(author_id))
+
+
+            if "!topreddit" in message_object.text:
+                
+                if "day" in message_object.text or "month" in message_object.text or "year" in message_object.text or 'all' in message_object.text or 'week' in message_object.text:
+                    reddit = []
+                    reddit = message_object.text.split()
+                    grab_pic(reddit[1], reddit[2], reddit[3])
+                    send_reddit_photos(reddit[1], author_id, thread_id, thread_type)
+                    delete_dir(reddit[1])
+                else:
+                    self.send(Message(text = 'Usage: ![topreddit] [subreddit] [top] [image_count]'), thread_id = thread_id, thread_type = thread_type)
+                
 
 
 
